59265cbf452b27416bfffc0ef4775d7ef7aeead1
Kagameow <kagami.404@gmail.com>
2024-08-04 23:14:37 +0200

    maxwell!

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,38 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color(0xFFFFFF);
+camera.position.z = 40;
+camera.position.y = 10;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+ambientLight.castShadow = true;
+scene.add(ambientLight);
+
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+const glTFLoader = new GLTFLoader();
+
+glTFLoader.load('src/assets/maxwell-the-cat-dingus/scene.gltf', async function (gltf) {
+    scene.add(gltf.scene);
+    renderer.render( scene, camera );
+    console.log(gltf);
+},undefined, function ( error ) {
+    console.error( error );
+});
+
+function animate() {
+    scene.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
